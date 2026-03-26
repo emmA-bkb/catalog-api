c2b4de2463b899593a583786b0bb1661bc7d578a
Emanoel Barbosa Boelter <emanoelboelter3@gmail.com>
2026-03-26 00:53:16 -0300

    database pelo render

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,26 +1,30 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from app.models.product import Base
 
 
 class Settings(BaseSettings):
-    
-    db_host: str = os.getenv("DB_HOST", "localhost")
-    db_port: int = int(os.getenv("DB_PORT", 5432))
-    db_name: str = os.getenv("DB_NAME", "api_db")
-    db_user: str = os.getenv("DB_USER", "postgres")
-    db_password: str = os.getenv("DB_PASSWORD", "postgres")
-    
-    class Config:
-        env_file = ".env"
-        case_sensitive = False
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_name: str = "api_db"
+    db_user: str = "postgres"
+    db_password: str = "postgres"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False
+    )
 
 
 settings = Settings()
 
-DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+DATABASE_URL = (
+    f"postgresql://{settings.db_user}:{settings.db_password}"
+    f"@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+)
+
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
